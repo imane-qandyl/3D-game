a349ab1eccd1aa22a3f9e02094fcc8ee59c95f3c
Imane Qandyl <imqandyl@lab1r1s2.42abudhabi.ae>
2025-04-27 08:18:11 +0400

    Added get_next_line as normal files

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imqandyl <imqandyl@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/22 09:24:35 by imqandyl          #+#    #+#             */
+/*   Updated: 2025/04/25 13:01:48 by imqandyl         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*strdup_to_newline(char *str)
+{
+	size_t	i;
+	char	*n_str;
+
+	i = 0;
+	if (!str || ft_strlen1(str) == 0)
+		return (NULL);
+	while (str[i] && str[i] != '\n')
+		i++;
+	if (str[i] == '\n')
+		i++;
+	n_str = (char *)malloc(i + 1);
+	if (!n_str)
+		return (NULL);
+	n_str[i] = '\0';
+	i = 0;
+	while (str[i] && str[i] != '\n')
+	{
+		n_str[i] = str[i];
+		i++;
+	}
+	if (str[i] == '\n')
+		n_str[i] = '\n';
+	return (n_str);
+}
+
+char	*read_chunks(int fd, char *str)
+{
+	char	buffer[BUFFER_SIZE + 1];
+	int		byte_read;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	byte_read = 1;
+	while (!ft_strchr1(str) && byte_read > 0)
+	{
+		byte_read = read(fd, buffer, BUFFER_SIZE);
+		if (byte_read < 0)
+		{
+			free(str);
+			return (NULL);
+		}
+		buffer[byte_read] = '\0';
+		str = ft_strjoin1(str, buffer);
+	}
+	return (str);
+}
+
+char	*remove_first_line(char *str)
+{
+	size_t	i;
+	char	*n_str;
+
+	i = 0;
+	while (str[i] && str[i] != '\n')
+		i++;
+	if (str[i] == '\n')
+		i++;
+	if (!str[i])
+	{
+		free(str);
+		return (NULL);
+	}
+	n_str = ft_strdup1(str + i);
+	free(str);
+	str = NULL;
+	if (!n_str)
+		return (NULL);
+	return (n_str);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*str;
+	char		*n_str;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	str = read_chunks(fd, str);
+	if (!str)
+		return (NULL);
+	n_str = strdup_to_newline(str);
+	if (!n_str)
+	{
+		free(str);
+		str = NULL;
+		return (NULL);
+	}
+	str = remove_first_line(str);
+	return (n_str);
+}
---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imqandyl <imqandyl@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/19 10:05:59 by imqandyl          #+#    #+#             */
+/*   Updated: 2025/04/25 13:01:10 by imqandyl         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+
+# endif
+
+# include <fcntl.h>
+# include <limits.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+int		ft_strchr1(char *s);
+size_t	ft_strlen1(char *s);
+char	*ft_strjoin1(char *s1, char *s2);
+char	*ft_strdup1(char *s);
+
+#endif
